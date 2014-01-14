cd7b58d4529b250242d0391fa00216fbe62dee5e
Florent <florent.david.lille1@gmail.com>
2014-01-14 12:25:36 +0100

    add Master.java

--- SID/src/sid/Master.java
+++ SID/src/sid/Master.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 public interface Master extends Remote {
 
 	public abstract Result doit(SetOfTasks s) throws RemoteException;
-	public abstract void delegate();
 	public abstract void gatherResult(Result r) throws RemoteException;
+	public abstract void inscription(Worker r) throws RemoteException;
 	
 }
