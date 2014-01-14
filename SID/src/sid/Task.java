2fe4e8a85fec52e8a5d7870cebb2a3b72e3b2542
Louis Billiet <louis.billiet59@gmail.com>
2014-01-14 10:36:10 +0100

    ajout de nouveaux trucs

---
+++ SID/src/sid/Task.java
@@ -0,0 +1,9 @@
+package sid;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
--- SID/src/sid/Worker.java
+++ SID/src/sid/Worker.java
@@ -1,5 +1,9 @@
 package sid;
 
+import java.rmi.Remote;
+
 public interface Worker extends Remote {
 
+	public Result compute(Task t);
+	
 }
