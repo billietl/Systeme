0358ae0a3d1ff024e10a4f64832b69b4aede029c
Louis Billiet <louis.billiet59@gmail.com>
2014-01-21 08:44:48 +0100

    ajout de l'implementation des interfaces

--- SID/src/sid/pull/MasterImpl.java
+++ SID/src/sid/pull/MasterImpl.java
@@ -1,5 +1,32 @@
 package sid.pull;
 
-public class MasterImpl {
+import java.rmi.RemoteException;
+import java.util.Collection;
+
+import sid.Master;
+import sid.Worker;
+import sid.api.Result;
+import sid.api.SetOfTasks;
+import sid.api.Task;
+
+public class MasterImpl implements Master{
+
+	@Override
+	public Result doit(SetOfTasks s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Task> gatherResult(Result r) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inscription(Worker r) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- SID/src/sid/pull/WorkerImpl.java
+++ SID/src/sid/pull/WorkerImpl.java
@@ -1,5 +1,20 @@
 package sid.pull;
 
-public class WorkerImpl {
+import java.rmi.RemoteException;
+import java.util.Collection;
+
+import sid.Worker;
+import sid.api.AggregationResults;
+import sid.api.Task;
+import sid.push.TooMuchWorkException;
+
+public class WorkerImpl implements Worker{
+
+	@Override
+	public void gatherTasks(Collection<Task> s, AggregationResults a)
+			throws RemoteException, TooMuchWorkException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
