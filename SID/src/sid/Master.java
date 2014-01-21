4e80b84ae017dcddf12818888513388dca0d7d10
Louis Billiet <louis.billiet59@gmail.com>
2014-01-21 08:28:24 +0100

    fini de scratcher le master

--- SID/src/sid/Master.java
+++ SID/src/sid/Master.java
@@ -2,11 +2,12 @@ package sid;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Collection;
 
 public interface Master extends Remote {
 
 	public abstract Result doit(SetOfTasks s) throws RemoteException;
-	public abstract void gatherResult(Result r) throws RemoteException;
+	public abstract Collection<Task> gatherResult(Result r) throws RemoteException;
 	public abstract void inscription(Worker r) throws RemoteException;
 	
 }
--- SID/src/sid/MasterImpl.java
+++ SID/src/sid/MasterImpl.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 public class MasterImpl extends UnicastRemoteObject implements Master {
 
+	private static final long serialVersionUID = 1L;
 	private List<Worker> workers;
 	private AggregationResults aggResults;
+	private SetOfTasks set;
 	private int lastTask;
-	public static final int TASK_SIZE_INDICE=2;
-	
+	public static final int CHUNK_SIZE_INDICE = 2;
+
 	protected MasterImpl() throws RemoteException {
 		super();
 		this.workers = new ArrayList<Worker>();
@@ -20,21 +22,40 @@ public class MasterImpl extends UnicastRemoteObject implements Master {
 
 	@Override
 	public Result doit(SetOfTasks s) throws RemoteException {
-		int subSize = s.getSize()/(workers.size()*TASK_SIZE_INDICE);
-		for(Worker r : workers) {
-			Collection<Task> taskSet = new ArrayList<Task>();
-			for(int i=lastTask; i<lastTask; i++){
-				taskSet.add(s.getTask(i));
+		this.set = s;
+		for (Worker r : workers) {
+			Collection<Task> taskSet = this.getTasks(lastTask, lastTask+getChunkSize());
+			try {
+				r.gatherTasks(taskSet, this.aggResults);
+				lastTask += getChunkSize();
+			} catch (TooMuchWorkException e) {
 			}
-			r.gatherTasks(taskSet, this.aggResults);
 		}
 		return null;
 	}
 
 	@Override
-	public void gatherResult(Result r) throws RemoteException {
-		// TODO Auto-generated method stub
-
+	public synchronized Collection<Task> gatherResult(Result r)	throws RemoteException {
+		this.aggResults.add(r);
+		if (this.lastTask < this.set.getSize()) {
+			return null;
+		} else {
+			Collection<Task> taskSet = this.getTasks(lastTask, lastTask+getChunkSize());
+			lastTask += getChunkSize();
+			return taskSet;
+		}
+	}
+	
+	private int getChunkSize(){
+		return this.set.getSize() / (workers.size() * CHUNK_SIZE_INDICE);
+	}
+	
+	private Collection<Task> getTasks(int i, int j){
+		Collection<Task> taskSet = new ArrayList<Task>();
+		for(;i<j;i++){
+			taskSet.add(this.set.getTask(i));
+		}
+		return taskSet;
 	}
 
 	@Override
