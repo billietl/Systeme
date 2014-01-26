886b3ace012c30ba6aaf6cb27c05aa9fdf3b9132
Louis Billiet <louis.billiet59@gmail.com>
2014-01-26 15:45:27 +0100

    gestion du nom du worker

--- SID/src/sid/pull/WorkerImpl.java
+++ SID/src/sid/pull/WorkerImpl.java
@@ -1,7 +1,9 @@
 package sid.pull;
 
 import java.net.InetAddress;
+import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Collection;
@@ -28,34 +30,58 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
 			try {
 				col = this.master.getTasks();
 				agg = this.master.getAggregationResult();
-				if(col.isEmpty()){
+				if (col.isEmpty()) {
 					Thread.sleep(100);
 					continue;
 				}
-				for(Task t : col){
+				for (Task t : col) {
 					agg.add(t.execute());
 				}
 				this.master.gatherResult(agg.getFinalResult());
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-			catch (RemoteException e) {e.printStackTrace();} 
-			catch (InterruptedException e) {e.printStackTrace();}
 		}
 	}
-	
-	public static void usage(){
+
+	public static void usage() {
 		System.out.println("Usage : WorkerImpl <adresse serveur rmi>");
 		System.exit(1);
+	}
+
+	public static int findLastId(int id, String url) {
+		try {
+			Naming.lookup("rmi://" + url + "/worker" + id + "@"
+					+ InetAddress.getLocalHost().getHostName());
+		} catch (NotBoundException e) {
+			return id;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return findLastId(id++, url);
 	}
 
 	public static void main(String args[]) {
 		/**
 		 * args[0] = adresse su serveur RMI
 		 */
-		if(args.length < 1) usage();
+		if (args.length < 1)
+			usage();
+		WorkerImpl worker = null;
+		Master m = null;
 		try {
-			MasterImpl m = ((MasterImpl) Naming.lookup("rmi://" + args[0] + "/master"));
-			WorkerImpl worker = new WorkerImpl(m);
-			Naming.bind("rmi://" + args[0] + "/worker@" + InetAddress.getLocalHost().getHostName(), worker);
+			m = ((Master) Naming.lookup("rmi://" + args[0] + "/master"));
+			int id = findLastId(0, args[0]);
+			id++;
+			worker = new WorkerImpl(m);
+			try {
+				Naming.bind("rmi://" + args[0] + "/worker" + id + "@"
+						+ InetAddress.getLocalHost().getHostName(), worker);
+			} catch (AlreadyBoundException e) {
+
+			}
 			worker.run();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -64,6 +90,7 @@ public class WorkerImpl extends UnicastRemoteObject implements Worker {
 
 	@Override
 	public void gatherTasks(Collection<Task> s, AggregationResults a)
-			throws RemoteException {}
+			throws RemoteException {
+	}
 
 }
