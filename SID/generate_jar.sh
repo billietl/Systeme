0c6b55be5dc756d705ba55cf66ec9cdf45623463
Louis Billiet <louis.billiet59@gmail.com>
2014-01-28 10:07:34 +0100

    re-compilation des sources

--- SID/generate_jar.sh
+++ SID/generate_jar.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 rm sid.jar 2>/dev/null
+javac -classpath src -d bin src/sid/pull/WorkerImpl.java &
+javac -classpath src -d bin src/sid/pull/MasterImpl.java &
+javac -classpath src -d bin src/sid/push/WorkerImpl.java &
+javac -classpath src -d bin src/sid/push/MasterImpl.java &
+javac -classpath src -d bin src/sid/integrale/ApplicationImpl.java &
 cd bin
 jar cf ../sid.jar ./*
 rmic -classpath ../sid.jar sid.pu{sh,ll}.{Master,Worker}Impl
