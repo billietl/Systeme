0a3850350abe41ae5ab7fa3b44be8b8f5ef3994d
Louis Billiet <louis.billiet59@gmail.com>
2014-02-11 11:35:45 +0100

    scripts pour aggreger les resultats de tests

--- SID/app.sh
+++ SID/app.sh
@@ -13,19 +13,27 @@ then
     usage
 fi
 
-let worker_numbers="`cat $OAR_FILE_NODES | wc -l`-1"
+echo $OAR_JOB_ID >> jobs
+
+echo "calcul des paramettres..."
+let worker_number="`cat $OAR_FILE_NODES | wc -l`-1"
 master=`cat $OAR_FILE_NODES | head -n 1` 
 workers=`cat $OAR_FILE_NODES | tail -n $worker_number`
 work_size=$1
 chunk_size=$2
 
-oarsh $master 'sh master.sh $chunk_size'
+echo "lancement du maitre..."
+oarsh $master "sh master.sh $chunk_size" &
+sleep 1
 
+echo "lancement des esclaves..."
 for host in $workers; do
-    oarsh $host 'sh worker.sh $master'
+    oarsh $host sh worker.sh $master
 done
+sleep 1
 
+echo "lancement du calcul..."
 time java -cp sid.jar sid.integrale.ApplicationImpl $master $work_size
-echo "nombre de noeuds " $worker_number
-echo "taille du travail " $work_size
-echo "taille des morceaux " $chunk_size
+echo -e "nombre de noeuds\t"${worker_number}
+echo -e "taille du travail\t"${work_size}
+echo -e "taille des morceaux\t"${chunk_size}
