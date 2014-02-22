3399177b80acbb26b208d3a41c023a629670d114
Louis Billiet <louis.billiet59@gmail.com>
2014-02-22 12:33:51 +0100

    ajout de resultats

--- SID/gather_results.sh
+++ SID/gather_results.sh
@@ -6,7 +6,7 @@ mv ../results.tgz .
 tar xvf results.tgz
 for job in `cat jobs`
 do
-    echo `cat OAR.$job.stdout|grep "nombre de noeuds"|cut -f 2`";"`cat OAR.$job.stdout|grep "taille du travail"|cut -f 2`";"`cat OAR.$job.stdout|grep "taille des morceaux"|cut -f 2`";"`cat OAR.$job.stderr|grep "real"|cut -f 2` >> ../results.csv
+    echo `cat OAR.$job.stdout|grep "nombre de noeuds"|cut -f 2`" "`cat OAR.$job.stdout|grep "taille des morceaux"|cut -f 2`" "`cat OAR.$job.stderr|grep "real"|cut -f 2`" "`cat OAR.$job.stdout|grep "taille du travail"|cut -f 2` >> ../results.csv
 done
 cd ..
 rm -r res
